a863dec8d5c2b1cd380383dacc63a24814d1bf62
Diana Craciun <diana.craciun@nxp.com>
2024-10-25 10:07:14 +0300

    Integrate the last version of the EL2GO Agent
    The EdgeLock 2GO Angent has changed the interface:
    - iot_agent_keystore_init, iot_agent_keystore_init
    become more generic.
    - status report printing is part of the EdgeLock 2GO
    library.
    
    Signed-off-by: Diana Craciun <diana.craciun@nxp.com>

--- src/oem_prov_online.c
+++ src/oem_prov_online.c
@@ -4,9 +4,8 @@
  */
 
 #include <nxp_iot_agent.h>
-#include <nxp_iot_agent_keystore_psa.h>
-#include <nxp_iot_agent_datastore_fs.h>
-#include <nxp_iot_agent_datastore_plain.h>
+#include <nxp_iot_agent_keystore.h>
+#include <nxp_iot_agent_datastore.h>
 #include <nxp_iot_agent_utils.h>
 
 #include "oem_prov.h"
@@ -17,168 +16,7 @@
 #define DATASTORE_EDGELOCK2GO_ID \
 	nxp_iot_DatastoreIdentifiers_DATASTORE_EDGELOCK2GO_ID
 
-/**
- * The report was taken as it is from the EdgeLock 2GO Agent code. In the
- * next Agent version this report should be exported as a library function.
- * Until then we keep it as it is although it has a different look and feel
- * from the rest of the code.
- */
-const char *
-update_status_report_description(nxp_iot_UpdateStatusReport_UpdateStatus status)
-{
-	switch (status) {
-	case nxp_iot_UpdateStatusReport_UpdateStatus_SUCCESS:
-		return "SUCCESS";
-	case nxp_iot_UpdateStatusReport_UpdateStatus_ERR_ENCODING:
-		return "ERR_ENCODING";
-	case nxp_iot_UpdateStatusReport_UpdateStatus_ERR_PROTOCOL:
-		return "ERR_PROTOCOL";
-	case nxp_iot_UpdateStatusReport_UpdateStatus_ERR_MEMORY_READ:
-		return "ERR_MEMORY_READ";
-	case nxp_iot_UpdateStatusReport_UpdateStatus_ERR_MEMORY_WRITE:
-		return "ERR_MEMORY_WRITE";
-	case nxp_iot_UpdateStatusReport_UpdateStatus_ERR_SSS_COMMUNICATION:
-		return "ERR_SSS_COMMUNICATION";
-	case nxp_iot_UpdateStatusReport_UpdateStatus_ERR_SSS_VERSION:
-		return "ERR_SSS_VERSION";
-	case nxp_iot_UpdateStatusReport_UpdateStatus_ERR_SSS_SECURE_CHANNEL:
-		return "ERR_SSS_SECURE_CHANNEL";
-	case nxp_iot_UpdateStatusReport_UpdateStatus_ERR_CONFIGURATION:
-		return "ERR_CONFIGURATION";
-	case nxp_iot_UpdateStatusReport_UpdateStatus_ERR_CONFIGURATION_TOO_MANY_DATASTORES:
-		return "ERR_CONFIGURATION_TOO_MANY_DATASTORES";
-	case nxp_iot_UpdateStatusReport_UpdateStatus_ERR_CONFIGURATION_TOO_MANY_KEYSTORES:
-		return "ERR_CONFIGURATION_TOO_MANY_KEYSTORES";
-	case nxp_iot_UpdateStatusReport_UpdateStatus_ERR_CONFIGURATION_SNI_MISSING:
-		return "ERR_CONFIGURATION_SNI_MISSING";
-	case nxp_iot_UpdateStatusReport_UpdateStatus_ERR_CONFIGURATION_SNI_INVALID:
-		return "ERR_CONFIGURATION_SNI_INVALID";
-	case nxp_iot_UpdateStatusReport_UpdateStatus_ERR_CONNECTION_QUOTA_EXCEEDED:
-		return "ERR_CONNECTION_QUOTA_EXCEEDED";
-	case nxp_iot_UpdateStatusReport_UpdateStatus_ERR_DEVICE_NOT_WHITELISTED:
-		return "ERR_DEVICE_NOT_WHITELISTED";
-	case nxp_iot_UpdateStatusReport_UpdateStatus_ERR_UPDATE_FAILED:
-		return "ERR_UPDATE_FAILED";
-	case nxp_iot_UpdateStatusReport_UpdateStatus_ERR_INTERNAL:
-		return "ERR_INTERNAL";
-	case nxp_iot_UpdateStatusReport_UpdateStatus_ERR_TIMEOUT:
-		return "ERR_TIMEOUT";
-	default:
-		return "UNKNOWN";
-	}
-}
-
-const char *
-claim_status_description(nxp_iot_AgentClaimStatus_ClaimStatus status)
-{
-	switch (status) {
-	case nxp_iot_AgentClaimStatus_ClaimStatus_SUCCESS:
-		return "SUCCESS";
-	case nxp_iot_AgentClaimStatus_ClaimStatus_ERR_NOT_FOUND:
-		return "ERR_NOT_FOUND";
-	case nxp_iot_AgentClaimStatus_ClaimStatus_ERR_WRONG_PRODUCT_TYPE:
-		return "ERR_WRONG_PRODUCT_TYPE";
-	case nxp_iot_AgentClaimStatus_ClaimStatus_ERR_CLAIM_CODE_REVOKED:
-		return "ERR_CLAIM_CODE_REVOKED";
-	case nxp_iot_AgentClaimStatus_ClaimStatus_ERR_CLAIM_CODE_LIMIT_REACHED:
-		return "ERR_CLAIM_CODE_LIMIT_REACHED";
-	case nxp_iot_AgentClaimStatus_ClaimStatus_ERR_CLAIM_CODE_REUSE_PROHIBITED:
-		return "ERR_CLAIM_CODE_REUSE_PROHIBITED";
-	case nxp_iot_AgentClaimStatus_ClaimStatus_ERR_CLAIM_CODE_READ:
-		return "ERR_CLAIM_CODE_READ";
-	case nxp_iot_AgentClaimStatus_ClaimStatus_ERR_CLAIM_CODE_POLICIES:
-		return "ERR_CLAIM_CODE_POLICIES";
-	case nxp_iot_AgentClaimStatus_ClaimStatus_ERR_CLAIM_CODE_TYPE:
-		return "ERR_CLAIM_CODE_TYPE";
-	case nxp_iot_AgentClaimStatus_ClaimStatus_ERR_CLAIM_FAILED:
-		return "ERR_CLAIM_FAILED";
-	case nxp_iot_AgentClaimStatus_ClaimStatus_ERR_CLAIM_CODE_FORMAT:
-		return "ERR_CLAIM_CODE_FORMAT";
-	case nxp_iot_AgentClaimStatus_ClaimStatus_ERR_TIMEOUT:
-		return "ERR_TIMEOUT";
-	case nxp_iot_AgentClaimStatus_ClaimStatus_ERR_INTERNAL:
-		return "ERR_INTERNAL";
-	default:
-		return "UNKNOWN";
-	}
-}
-
-const char *rtp_status_description(nxp_iot_AgentRtpStatus_RtpStatus status)
-{
-	switch (status) {
-	case nxp_iot_AgentRtpStatus_RtpStatus_SUCCESS:
-		return "SUCCESS";
-	case nxp_iot_AgentRtpStatus_RtpStatus_SUCCESS_NO_CHANGE:
-		return "SUCCESS_NO_CHANGE";
-	case nxp_iot_AgentRtpStatus_RtpStatus_ERR_RTP_FAILED:
-		return "ERR_RTP_FAILED";
-	case nxp_iot_AgentRtpStatus_RtpStatus_ERR_OBJECT_ATTRIBUTES_READ_FAILED:
-		return "ERR_OBJECT_ATTRIBUTES_READ_FAILED";
-	case nxp_iot_AgentRtpStatus_RtpStatus_ERR_OBJECT_DELETE_FAILED:
-		return "ERR_OBJECT_DELETE_FAILED";
-	case nxp_iot_AgentRtpStatus_RtpStatus_ERR_OBJECT_WRITE_FAILED:
-		return "ERR_OBJECT_WRITE_FAILED";
-	case nxp_iot_AgentRtpStatus_RtpStatus_ERR_DEFECTIVE:
-		return "ERR_DEFECTIVE";
-	case nxp_iot_AgentRtpStatus_RtpStatus_ERR_CURVE_INSTALLATION_FAILED:
-		return "ERR_CURVE_INSTALLATION_FAILED";
-	case nxp_iot_AgentRtpStatus_RtpStatus_ERR_TIMEOUT:
-		return "ERR_TIMEOUT";
-	case nxp_iot_AgentRtpStatus_RtpStatus_ERR_INTERNAL:
-		return "ERR_INTERNAL";
-	default:
-		return "UNKNOWN";
-	}
-}
-
-void print_status_report(const nxp_iot_UpdateStatusReport *status_report)
-{
-	OEM_PROV_PRINTF("Update status report:\n");
-	OEM_PROV_PRINTF(
-		"  The device update %s (0x%04x: %s)\n",
-		(status_report->status ==
-				 nxp_iot_UpdateStatusReport_UpdateStatus_SUCCESS ?
-			 "was successful" :
-			 "FAILED"),
-		status_report->status,
-		update_status_report_description(status_report->status));
-	OEM_PROV_PRINTF("  The correlation-id for this update is %s.\n",
-			status_report->correlationId);
-
-	if (status_report->has_claimStatus) {
-		OEM_PROV_PRINTF(
-			"  Status for claiming the device: 0x%04x: %s.\n",
-			status_report->claimStatus.status,
-			claim_status_description(
-				status_report->claimStatus.status));
-		for (size_t i = 0U;
-		     i < status_report->claimStatus.details_count; i++) {
-			nxp_iot_AgentClaimStatus_DetailedClaimStatus *s =
-				&status_report->claimStatus.details[i];
-			OEM_PROV_PRINTF(
-				"    On endpoint 0x%08x, status for claiming: 0x%04x: %s.\n",
-				s->endpointId, s->status,
-				claim_status_description(s->status));
-		}
-	}
-
-	if (status_report->has_rtpStatus) {
-		OEM_PROV_PRINTF(
-			"  Status for remote trust provisioning: 0x%04x: %s.\n",
-			status_report->rtpStatus.status,
-			rtp_status_description(
-				status_report->rtpStatus.status));
-		for (size_t i = 0U; i < status_report->rtpStatus.details_count;
-		     i++) {
-			nxp_iot_AgentRtpStatus_RtpObjectStatus *s =
-				&status_report->rtpStatus.details[i];
-			OEM_PROV_PRINTF(
-				"    On endpoint 0x%08x, for object 0x%08x, status: 0x%04x: %s.\n",
-				s->endpointId, s->objectId, s->status,
-				rtp_status_description(s->status));
-		}
-	}
-}
+const char *el2go_datastore = "edgelock2go_datastore.bin";
 
 int oem_prov_online(void)
 {
@@ -187,8 +25,17 @@ int oem_prov_online(void)
 	iot_agent_context_t iot_agent_context = { 0 };
 	iot_agent_keystore_t keystore = { 0 };
 	iot_agent_datastore_t el2go_data = { 0 };
+	iot_agent_platform_context_t iot_agent_platform_context = { 0 };
 	nxp_iot_UpdateStatusReport status_report =
 		nxp_iot_UpdateStatusReport_init_default;
+
+	OEM_PROV_DBG_PRINTF(VERBOSE, "IOT agent platform init\n");
+	agent_status =
+		iot_agent_platform_init(0, NULL, &iot_agent_platform_context);
+	if (agent_status != IOT_AGENT_SUCCESS) {
+		status = OEM_PROV_STATUS_EL2GO_AGENT_ERROR;
+		goto exit;
+	}
 
 	OEM_PROV_DBG_PRINTF(VERBOSE, "IOT agent init\n");
 	agent_status = iot_agent_init(&iot_agent_context);
@@ -198,8 +45,9 @@ int oem_prov_online(void)
 	}
 
 	OEM_PROV_DBG_PRINTF(VERBOSE, "keystore psa init\n");
-	agent_status =
-		iot_agent_keystore_psa_init(&keystore, EDGELOCK2GO_KEYSTORE_ID);
+	agent_status = iot_agent_keystore_init(&keystore,
+					       EDGELOCK2GO_KEYSTORE_ID,
+					       &iot_agent_platform_context);
 	if (agent_status != IOT_AGENT_SUCCESS) {
 		status = OEM_PROV_STATUS_EL2GO_AGENT_ERROR;
 		goto exit;
@@ -214,8 +62,9 @@ int oem_prov_online(void)
 	}
 
 	OEM_PROV_DBG_PRINTF(VERBOSE, "agent datastore plain init\n");
-	agent_status = iot_agent_datastore_plain_init(&el2go_data,
-						      DATASTORE_EDGELOCK2GO_ID);
+	agent_status = iot_agent_datastore_init(&el2go_data,
+						DATASTORE_EDGELOCK2GO_ID, el2go_datastore,
+						&iot_agent_service_is_configuration_data_valid);
 	if (agent_status != IOT_AGENT_SUCCESS) {
 		status = OEM_PROV_STATUS_EL2GO_AGENT_ERROR;
 		goto exit;
@@ -243,15 +92,9 @@ int oem_prov_online(void)
 							     &status_report);
 
 	if (status_report.has_status)
-		print_status_report(&status_report);
+		iot_agent_print_status_report(&status_report);
 
 	if (agent_status != IOT_AGENT_SUCCESS) {
-		status = OEM_PROV_STATUS_EL2GO_AGENT_ERROR;
-		goto exit;
-	}
-
-	if (!iot_agent_is_service_configuration_data_valid(&iot_agent_context)) {
-		OEM_PROV_DBG_PRINTF(ERROR, "Invalid configurations detected\n");
 		status = OEM_PROV_STATUS_EL2GO_AGENT_ERROR;
 		goto exit;
 	}
